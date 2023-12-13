82258c5812e95a15d8c00e7777d52da74d992fc1
Anis FETOUI <anis.fetoui@esprit.tn>
2023-12-14 00:03:11 +0100

    add a dotenv package and connect it to my app

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 const bGround = require('fcc-express-bground');
+require('dotenv').config();
 
 bGround.log("Hello World");
 console.log("Hello World");
@@ -12,11 +13,25 @@ app.get("/",(req,res) => {
 
 app.use('/public', express.static(__dirname + "/public"));
 
+// app.get("/json",(req,res) => {
+//     res.json(
+//         {"message" : "Hello json"}
+//     );
+// });
+
 app.get("/json",(req,res) => {
+  if(process.env.MESSAGE_STYLE === "uppercase")
+  {res.json(
+      {"message" : "HELLO JSON"}
+  );
+}else{
     res.json(
-        {"message" : "Hello json"}
-    );
+      {"message" : "Hello json"}
+  );
+  }
 });
+
+
 
 
 
