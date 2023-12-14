52432e5590b84c55a2b498bc5488d54e1b079694
Anis FETOUI <anis.fetoui@esprit.tn>
2023-12-14 01:06:36 +0100

    add a middlware logger

--- myApp.js
+++ myApp.js
@@ -3,6 +3,15 @@ let app = express();
 const bGround = require('fcc-express-bground');
 require('dotenv').config();
 
+
+
+
+app.use((req,res,next)=>{
+  console.log(req.method +" " +req.path+ " - " + req.ip);
+  next();
+})
+
+
 bGround.log("Hello World");
 console.log("Hello World");
 
@@ -13,23 +22,28 @@ app.get("/",(req,res) => {
 
 app.use('/public', express.static(__dirname + "/public"));
 
+
+
 // app.get("/json",(req,res) => {
+//   if(process.env.MESSAGE_STYLE === "uppercase")
+//   {res.json(
+//       {"message" : "HELLO JSON"}
+//   );
+// }else{
 //     res.json(
-//         {"message" : "Hello json"}
-//     );
+//       {"message" : "Hello json"}
+//   );
+//   }
 // });
 
 app.get("/json",(req,res) => {
+  var jsonResponse ={ "message":"Hello json" };
   if(process.env.MESSAGE_STYLE === "uppercase")
-  {res.json(
-      {"message" : "HELLO JSON"}
-  );
-}else{
-    res.json(
-      {"message" : "Hello json"}
-  );
+  {
+    jsonResponse.message = jsonResponse.message.toUpperCase();
   }
-});
+  res.json(jsonResponse);
+  });
 
 
 
